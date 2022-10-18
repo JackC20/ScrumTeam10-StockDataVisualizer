7ef672b5f274b701fc6eeda2ea199e1d1104eab4
Matt Hippe <waterski2000@gmail.com>
2022-10-18 16:52:48 -0500

    First three inputs with error checking

--- StockVisualizer.py
+++ StockVisualizer.py
@@ -1,1 +1,27 @@
-
+print("Stock Data Visualizer\n---------------------\n")
+stockName = input("Enter the stock symbol you are looking for: ")
+print("Chart Types\n-----------\n1. Bar\n2. Line\n")
+chartType = input("Enter the chart type you want (1, 2): ")
+while(True):
+    try:
+        chartType = int(input("Enter the chart type you want (1, 2): "))
+        if chartType != 1 and chartType != 2:
+            print("Enter a 1 or 2 for chart type")
+            continue
+    except:
+        print("Enter a 1 or 2 for chart type")
+        continue
+    else:
+        break
+print("Select the time series of the chart you want to Generate\n--------------------------------------------------------\n1. Intraday\n2. Daily\n3. Weekly\n4. Monnthly")
+while(True):
+    try:
+        timeSeries = int(input("Enter time series option (1, 2, 3, 4): "))
+        if timeSeries != 1 and timeSeries != 2 and timeSeries != 3 and timeSeries != 4:
+            print("Enter either 1, 2, 3, or 4 for time series")
+            continue
+    except:
+        print("Enter either 1, 2, 3, or 4")
+        continue
+    else:
+        break
