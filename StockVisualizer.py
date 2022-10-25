da85d178d09159da1ec591d4d4eefa5833de06c7
jpd4fn <60529599+jpd4fn@users.noreply.github.com>
2022-10-25 16:18:52 -0500

    Update StockVisualizer.py

--- StockVisualizer.py
+++ StockVisualizer.py
@@ -1,1 +1,33 @@
 
+import pygal
+from lxml import *
+
+class graphResults:
+    def graph(data, graphType):
+        #data is passed to this function as a dictionary of api results. 
+        dateLessData = data.values()
+        opens = []
+        highs = []
+        lows = []
+        close = []
+
+        for x in dateLessData:
+            opens.append(float(x.get("1. open")))
+            highs.append(float(x.get("2. high")))
+            lows.append(float(x.get("3. low")))
+            close.append(float(x.get("4. close")))
+
+        if graphType == 0:
+            chart = pygal.Bar()
+            chart.add("Opens", opens)
+            chart.add("Highs", highs)
+            chart.add("Lows", lows)
+            chart.add("Closes", close)
+            chart.render_in_browser()
+        else:
+            chart = pygal.Line()
+            chart.add("Opens", opens)
+            chart.add("Highs", highs)
+            chart.add("Lows", lows)
+            chart.add("Closes", close)
+            chart.render_in_browser()
